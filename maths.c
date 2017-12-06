1206d5af7f4f9555fa0a3840dcf4bfa2e18e0c97
Hector Wallen <hwallen@atex.com>
2017-12-06 18:19:55 +0100

    points is now realloced, passes valgrind

--- maths.c
+++ maths.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define STRING_SIZE 128
-#define MALLOC_SEG
+#define MALLOC_SEG 1024
 
 typedef struct point {
   double data;
@@ -61,47 +61,51 @@ double avg(double* points, short point_len) {
   return (double)sum / point_len;
 }
 
-int readkeystruct(double* points, short point_len, point** pointbuf_ptr) {
+int readkeystruct(double** points, short point_len, point** pointbuf) {
   char* key;
   short i = 0;
-  point* pointbuf = *pointbuf_ptr;
   int exitcode;
-  if(strlen(pointbuf->key) < 1) {
-    readdata(pointbuf);
+  if(strlen((*pointbuf)->key) < 1) {
+    readdata(*pointbuf);
   } 
 
-  key = malloc((strlen(pointbuf->key) + 1) * sizeof(char));
-  strcpy(key, pointbuf->key);
-  *points = pointbuf->data;
+  key = malloc((strlen((*pointbuf)->key) + 1) * sizeof(char));
+  strcpy(key, (*pointbuf)->key);
+  **points = (*pointbuf)->data;
   ++i;
 
   //puts(key);
 
 
-  while ((exitcode = readdata(pointbuf)) != 1) {
-    if(strcmp(key, pointbuf->key)) {
+  while ((exitcode = readdata(*pointbuf)) != 1) {
+    printf("%d %d\n", i, point_len);
+    if(i + 1 >= point_len ) {
+      *points = realloc(points, point_len + MALLOC_SEG);
+      point_len += MALLOC_SEG;
+    }
+    if(strcmp(key, (*pointbuf)->key)) {
       break;
     }
-    *(points + i) = pointbuf->data;
+    *(*points + i) = (*pointbuf)->data;
     i++;
   }
   //for (int k = 0; k < i; ++k) {
   //  printf("s=%lf\n", *(points + k));
   //}
 
-  printf("%s %lf\n", key, avg(points, i));
+  printf("%s %lf\n", key, avg(*points, i));
   free(key);
   return exitcode;
 }
 
 int main() {
-  double* points = malloc(sizeof(double) * 5000);
-  short len = 5000;
+  double* points = malloc(sizeof(double) * 1);
+  short len = 1;
   point* pointbuf;
   pointbuf = malloc(sizeof(point));
   pointbuf->key = malloc(sizeof(char) * STRING_SIZE * 5000);
   *(pointbuf->key) = '\0';
-  while (readkeystruct(points, len, &pointbuf) != 1) {
+  while (readkeystruct(&points, len, &pointbuf) != 1) {
     (void)0;
   }
   free(pointbuf->key);
