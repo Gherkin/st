f9ace23458580fafca1f6a9c984a111ef4b75c72
Matte <mattias4@kth.se>
2018-01-13 01:41:47 +0100

    Added implementation that skips copying lines

--- st.cpp
+++ st.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <string.h>
+
+const int BUF_SIZE = 4096;
 
 double median(std::vector<double> &v) {
     // 2 x nth_element was a tiny bit faster for my data
@@ -9,10 +12,9 @@ double median(std::vector<double> &v) {
     size_t n = v.size() / 2;
     nth_element(v.begin(), v.begin() + n, v.end());
     double median = v[n];
-    if (v.size() % 2 == 0)
-    {
+    if (v.size() % 2 == 0) {
         nth_element(v.begin(), v.begin() + n - 1, v.end());
-        median = (v[n-1] + median) / 2.;
+        median = (v[n - 1] + median) / 2.;
     }
 
     return median;
@@ -22,23 +24,50 @@ double mean(std::vector<double> &v) {
     return std::accumulate(v.begin(), v.end(), .0) / v.size();
 }
 
-int main() {
-    char buf[4096];
-    char key[4096];
-    strcpy(key, "");
-    std::vector<double> numbers;
-    while (fgets(buf, sizeof buf, stdin)) {
-        char *s = strtok(buf, " ");
-        if (strcmp(key, s)) {  // New key
-            if (!numbers.empty()) {
-                printf("%s %lf %lf\n", key, mean(numbers), median(numbers));
-            }
-            numbers.clear();
-            strcpy(key, s);
-        }
-
-        s = strtok(NULL, "");
-        numbers.push_back(atof(s));
+double finish_line(char *buf, const char *s) {
+    strcpy(buf, s);
+    char c, *tmp = buf + strlen(s);
+    while (c = getc_unlocked(stdin), c != EOF && c != '\n') {
+        *tmp++ = c;
     }
+    *tmp = '\0';
+    return atof(tmp);
 }
 
+int main() {
+    char buf[BUF_SIZE], key[BUF_SIZE];
+    int nread, prefix = 0;
+
+    strcpy(key, "");
+    std::vector<double> numbers;
+    while (nread = fread(buf + prefix, 1, sizeof buf - prefix - 1, stdin), nread) {
+        buf[nread + prefix] = '\0';
+        prefix = 0;
+        char *n = buf;
+        while (true) {
+            char *s = strsep(&n, " ");
+            if (!n) {
+                // End of buffer while reading key, put the few chars back and start over...
+                strcpy(buf, s);
+                prefix = strlen(s);
+                break;
+            }
+
+            if (strcmp(key, s)) {
+                if (!numbers.empty()) {
+                    printf("%s %lf %lf\n", key, mean(numbers), median(numbers));
+                }
+                numbers.clear();
+                strcpy(key, s);
+            }
+
+            s = strsep(&n, "\n");
+            if (!n) {
+                double f = finish_line(buf, s);
+                numbers.push_back(f);
+                break;
+            }
+            numbers.push_back(atof(s));
+        }
+    }
+}
